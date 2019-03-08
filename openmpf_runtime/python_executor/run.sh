43261b20b68fedabcd8e7d41f4e8260112b4c25c
brosenberg <brosenberg@mitre.org>
2019-03-08 09:28:46 -0500

    WIP: Standalone Python executor

---
+++ openmpf_runtime/python_executor/run.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+
+set -e
+set -x
+
+# After install.tar is rebuilt, the hard link (unlike a symlink) will still be pointing to the previous version of
+# install.tar. In other words `rm ../build_artifacts/install.tar` has no effect on ./install.tar
+if ! [ "install.tar" -ef "../build_artifacts/install.tar" ]; then
+    rm -f install.tar ||:
+    # TODO: Find better way to exclude build artifacts other than install.tar (maybe .dockerignore?)
+    # The other files in build_artifacts make the "Sending build context to Docker daemon" step take too long.
+    # Need to use hard link because Docker doesn't support symlinks outside of build context
+    sudo ln ../build_artifacts/install.tar install.tar
+fi
+
+
+
+# TODO: Use openmpf_runtime as build context
+#cd /home/mpf/openmpf-docker/openmpf_runtime
+#docker build . -f python_executor/Dockerfile -t python_executor --build-arg "COMPONENT_NAME=$COMPONENT_NAME"
+
+cd /home/mpf/openmpf-docker/openmpf_runtime/python_executor
+docker build . -t python_executor
+
+component_dir=/home/mpf/openmpf-projects/openmpf-python-component-sdk/detection/examples/PythonOcvComponent/
+
+#docker run --rm -it \
+#    --network host \
+#    -v "$component_dir:/component" \
+#    -v "$MPF_HOME/share/remote-media:$MPF_HOME/share/remote-media" \
+#    python_executor
+#
+#docker run --rm -it \
+#    --network openmpf_default \
+#    -v "$component_dir:/component" \
+#    -v openmpf_shared_data:/opt/mpf/share \
+#    python_executor
+
+
