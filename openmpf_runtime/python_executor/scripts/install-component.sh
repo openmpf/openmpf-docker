fd5b0126971fbb882d45a39ee50114ec1890e1fc
brosenberg <brosenberg@mitre.org>
2019-04-02 09:07:26 -0400

    WIP: Standalone Python executor - Add support for component provided pre-built whl files

--- openmpf_runtime/python_executor/scripts/install-component.sh
+++ openmpf_runtime/python_executor/scripts/install-component.sh
@@ -3,11 +3,21 @@
 set -e
 set -x
 
-if [ ! -r /home/mpf/component_src/setup.py ]; then
+
+if [ ! -e /home/mpf/component_src/setup.py ]; then
     echo 'Error expected /home/mpf/component_src/setup.py to exist'
     exit 4
 fi
 
 mkdir -p "$MPF_HOME/plugins/plugin"
 cp -r /home/mpf/component_src/plugin-files/* "$MPF_HOME/plugins/plugin/"
-"$COMPONENT_VIRTUALENV/bin/pip" install --no-cache-dir /home/mpf/component_src
+
+
+if [ -d /home/mpf/component_src/plugin-files/wheelhouse ]; then
+    "$COMPONENT_VIRTUALENV/bin/pip" install \
+        --find-links /home/mpf/component_src/plugin-files/wheelhouse \
+        --no-cache-dir /home/mpf/component_src
+else
+    "$COMPONENT_VIRTUALENV/bin/pip" install \
+        --no-cache-dir /home/mpf/component_src
+fi
