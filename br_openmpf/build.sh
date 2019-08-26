72ac13b79f164e700f2fd92fc7d58a50f665c57b
Brian Rosenberg <brosenberg@mitre.org>
2019-08-26 16:36:06 -0400

    No component build working.

---
+++ br_openmpf/build.sh
@@ -0,0 +1,22 @@
+
+set -e
+set -x
+
+cd /home/mpf/openmpf-docker/br_openmpf
+
+echo '=== OpenMPF Build ==='
+
+docker build -f openmpf_build/Dockerfile /home/mpf/openmpf-projects \
+  -t br-openmpf-build
+
+sleep 2
+
+echo '=== Workflow Manager ==='
+docker build workflow-manager -t br-workflow-manager
+
+
+echo '=== Node Manager ==='
+docker build node-manager -t br-node-manager
+
+
+# docker run --rm -it --network br_openmpf_overlay -v br_openmpf_shared_data:/opt/mpf/share -e WFM_USER=admin -e WFM_PASSWORD=mpfadm fdc72491382923aa47
---
+++ br_openmpf/workflow-manager/docker-entrypoint.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e
+set -x
+
+
+set +o xtrace
+
+# Wait for mySQL service.
+echo "Waiting for MySQL to become available ..."
+until mysql -h "$MYSQL_HOST" -u root -p"$MYSQL_ROOT_PASSWORD" -e "quit" >> /dev/null 2>&1; do
+  echo "MySQL is unavailable. Sleeping."
+  sleep 5
+done
+echo "MySQL is up"
+
+# Wait for Redis service.
+echo "Waiting for Redis to become available ..."
+# From https://stackoverflow.com/a/39214806
+until [ +PONG = "$( (exec 8<>/dev/tcp/redis/6379 && echo -e 'PING\r\n' >&8 && head -c 5 <&8; exec 8>&-) 2>/dev/null )" ]; do
+  echo "Redis is unavailable. Sleeping."
+  sleep 5
+done
+echo "Redis is up"
+
+# Wait for ActiveMQ service.
+echo "Waiting for ActiveMQ to become available ..."
+until curl -I "$ACTIVE_MQ_HOST:8161" >> /dev/null 2>&1; do
+  echo "ActiveMQ is unavailable. Sleeping."
+  sleep 5
+done
+echo "ActiveMQ is up"
+
+set -o xtrace
+
+
+
+/opt/apache-tomcat/bin/catalina.sh run
