aa8f0ea7f0ff17ba3c11acc135c5c91fcfb14b85
Brian Rosenberg <brosenberg@mitre.org>
2019-09-23 14:39:56 -0400

    Use wait-for-it instead of mysql binary.

--- br_openmpf/integration-tests/docker-entrypoint.sh
+++ br_openmpf/integration-tests/docker-entrypoint.sh
@@ -3,15 +3,15 @@
 set -Ee -o pipefail -o xtrace
 
 updateOrAddProperty() {
-  file="$1"
-  key="$2"
-  value="$3"
+    file="$1"
+    key="$2"
+    value="$3"
 
-  if grep -q "^$key=" "$file"; then
-    sed -i "/$key=/s/=.*/=$value/" "$file"
-  else
-    echo "$key=$value" >> "$file"
-  fi
+    if grep -q "^$key=" "$file"; then
+        sed -i "/$key=/s/=.*/=$value/" "$file"
+    else
+        echo "$key=$value" >> "$file"
+    fi
 }
 mkdir -p "$MPF_HOME/share/config"
 
@@ -29,28 +29,25 @@ cp -r trunk/mpf-system-tests/src/test/resources/samples/* "$MPF_HOME/share/sampl
 cp -r trunk/workflow-manager/src/test/resources/samples/* "$MPF_HOME/share/samples"
 
 
-set +o xtrace
 echo "Waiting for MySQL to become available ..."
-until mysql -h "$MYSQL_HOST" -u root -p"$MYSQL_ROOT_PASSWORD" -e "quit" >> /dev/null 2>&1; do
-  echo "MySQL is unavailable. Sleeping."
-  sleep 5
-done
-echo "MySQL is up"
+/scripts/wait-for-it.sh "$MYSQL_HOST:3306" --timeout=0
+echo 'MySQL is up'
+
 
 # Wait for Redis service.
 echo "Waiting for Redis to become available ..."
 # From https://stackoverflow.com/a/39214806
 until [ +PONG = "$( (exec 8<>/dev/tcp/redis/6379 && echo -e 'PING\r\n' >&8 && head -c 5 <&8; exec 8>&-) 2>/dev/null )" ]; do
-  echo "Redis is unavailable. Sleeping."
-  sleep 5
+    echo "Redis is unavailable. Sleeping."
+    sleep 5
 done
 echo "Redis is up"
 
 # Wait for ActiveMQ service.
 echo "Waiting for ActiveMQ to become available ..."
 until curl -I "$ACTIVE_MQ_HOST:8161" >> /dev/null 2>&1; do
-  echo "ActiveMQ is unavailable. Sleeping."
-  sleep 5
+    echo "ActiveMQ is unavailable. Sleeping."
+    sleep 5
 done
 echo "ActiveMQ is up"
 
@@ -93,9 +90,9 @@ find . -path '*/failsafe-reports/*.xml' -exec cp {} /test-reports/failsafe-repor
 
 
 if [ "$mavenRetVal" -eq 0 ]; then
-  echo 'DETECTED MAVEN TESTS PASSED'
+    echo 'DETECTED MAVEN TESTS PASSED'
 else
-  echo 'DETECTED MAVEN TEST FAILURE(S)'
+    echo 'DETECTED MAVEN TEST FAILURE(S)'
 fi
 
 exit "$mavenRetVal"
--- br_openmpf/workflow-manager/docker-entrypoint.sh
+++ br_openmpf/workflow-manager/docker-entrypoint.sh
@@ -4,15 +4,14 @@ set -e
 set -x
 
 updateOrAddProperty() {
-  file="$1"
-  key="$2"
-  value="$3"
-
-  if grep -q "^$key=" "$file"; then
-    sed -i "/$key=/s/=.*/=$value/" "$file"
-  else
-    echo "$key=$value" >> "$file"
-  fi
+    file="$1"
+    key="$2"
+    value="$3"
+    if grep -q "^$key=" "$file"; then
+        sed -i "/$key=/s/=.*/=$value/" "$file"
+    else
+        echo "$key=$value" >> "$file"
+    fi
 }
 
 
@@ -24,30 +23,26 @@ updateOrAddProperty "$mpfCustomPropertiesFile" "node.auto.config.enabled" "true"
 updateOrAddProperty "$mpfCustomPropertiesFile" "node.auto.unconfig.enabled" "true"
 
 
-set +o xtrace
-
-# Wait for mySQL service.
 echo "Waiting for MySQL to become available ..."
-until mysql -h "$MYSQL_HOST" -u root -p"$MYSQL_ROOT_PASSWORD" -e "quit" >> /dev/null 2>&1; do
-  echo "MySQL is unavailable. Sleeping."
-  sleep 5
-done
-echo "MySQL is up"
+/scripts/wait-for-it.sh "$MYSQL_HOST:3306" --timeout=0
+echo 'MySQL is available'
 
+
+set +o xtrace
 # Wait for Redis service.
 echo "Waiting for Redis to become available ..."
 # From https://stackoverflow.com/a/39214806
 until [ +PONG = "$( (exec 8<>/dev/tcp/redis/6379 && echo -e 'PING\r\n' >&8 && head -c 5 <&8; exec 8>&-) 2>/dev/null )" ]; do
-  echo "Redis is unavailable. Sleeping."
-  sleep 5
+    echo "Redis is unavailable. Sleeping."
+    sleep 5
 done
 echo "Redis is up"
 
 # Wait for ActiveMQ service.
 echo "Waiting for ActiveMQ to become available ..."
 until curl -I "$ACTIVE_MQ_HOST:8161" >> /dev/null 2>&1; do
-  echo "ActiveMQ is unavailable. Sleeping."
-  sleep 5
+    echo "ActiveMQ is unavailable. Sleeping."
+    sleep 5
 done
 echo "ActiveMQ is up"
 
