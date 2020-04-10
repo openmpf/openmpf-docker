06bbe3c7ba2de7fe51dec4d348bbb2b56cb93526
jrobble <jrobble@mitre.org>
2020-04-10 10:20:00 -0400

    Address PR comments.

---
+++ openmpf_runtime/node_manager/docker-entrypoint.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/bash
+
+#############################################################################
+# NOTICE                                                                    #
+#                                                                           #
+# This software (or technical data) was produced for the U.S. Government    #
+# under contract, and is subject to the Rights in Data-General Clause       #
+# 52.227-14, Alt. IV (DEC 2007).                                            #
+#                                                                           #
+# Copyright 2019 The MITRE Corporation. All Rights Reserved.                #
+#############################################################################
+
+#############################################################################
+# Copyright 2019 The MITRE Corporation                                      #
+#                                                                           #
+# Licensed under the Apache License, Version 2.0 (the "License");           #
+# you may not use this file except in compliance with the License.          #
+# You may obtain a copy of the License at                                   #
+#                                                                           #
+#    http://www.apache.org/licenses/LICENSE-2.0                             #
+#                                                                           #
+# Unless required by applicable law or agreed to in writing, software       #
+# distributed under the License is distributed don an "AS IS" BASIS,        #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  #
+# See the License for the specific language governing permissions and       #
+# limitations under the License.                                            #
+#############################################################################
+
+#!/usr/bin/bash
+
+set -Ee -o pipefail -o xtrace
+
+export THIS_MPF_NODE="${THIS_MPF_NODE}_id_${HOSTNAME}"
+export JGROUPS_TCP_ADDRESS=$HOSTNAME
+
+/scripts/wait-for-it.sh workflow_manager:8080 --timeout=0
+
+exec java -jar "${MPF_HOME}"/jars/mpf-nodemanager-*.jar
