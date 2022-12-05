b345f8c86692f415e76a32fd8bd315c08ba01479
jrobble <jrobble@mitre.org>
2022-12-05 14:05:49 -0500

    Download any missing mvn dependencies before performing tests.

--- integration_tests/docker-entrypoint.sh
+++ integration_tests/docker-entrypoint.sh
@@ -93,6 +93,10 @@ set -o xtrace
 
 set +o errexit
 
+# Download any missing dependencies, such as those used for custom system tests, to prevent potential
+# download timeout issues later on that occur on some host networks.
+mvn org.apache.maven.plugins:maven-dependency-plugin:3.1.1:go-offline
+
 mvn verify -Pjenkins \
     -Dspring.profiles.active=docker,jenkins \
     -Dit.test=ITWebREST \
